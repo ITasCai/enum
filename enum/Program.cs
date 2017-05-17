d6f72938ae17ac1614fe8a2fc956bc5d04eabacd
孤独的蔬菜 <shuai19970217@163.com>
2017-05-17 10:46:25 +0800

    枚举

--- enum/Program.cs
+++ enum/Program.cs
@@ -4,24 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace @enum
+namespace EnumDeam
+
 {
     enum color {
         yellow,brown=10,blue,black,purple
+    }
+
+    enum Season {
+        spring,summer,autumn,winter
     }
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("yellow={0}",color.yellow);
-            Console.WriteLine("yellow={0}", (int)color.yellow);
-            Console.WriteLine("brown={0}",(int)color.brown);
-            Console.WriteLine("blue={0}", (int)color.blue);
-            Console.WriteLine("black={0}", (int)color.black);
-            Console.WriteLine("purple={0}", (int)color.purple);
+            //Console.WriteLine("yellow={0}",color.yellow);
+            //Console.WriteLine("yellow={0}", (int)color.yellow);
+            //Console.WriteLine("brown={0}",(int)color.brown);
+            //Console.WriteLine("blue={0}", (int)color.blue);
+            //Console.WriteLine("black={0}", (int)color.black);
+            //Console.WriteLine("purple={0}", (int)color.purple);
 
-            Console.ReadKey();
+            //Console.ReadKey();
+
+            Console.WriteLine("输入一个月份：");
+            int yue = Convert.ToInt32(Console.ReadLine());
+            switch (yue)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine(Season.spring);
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine(Season.summer);
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine(Season.autumn);
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine(Season.winter);
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
