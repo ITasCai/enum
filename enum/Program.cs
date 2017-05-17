6b065e8d933cec18f646633af2341a47102ddb72
孤独的蔬菜 <shuai19970217@163.com>
2017-05-17 10:29:45 +0800

    枚举

--- enum/Program.cs
+++ enum/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace @enum
 {
+    enum color {
+        yellow,brown=10,blue,black,purple
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("yellow={0}",color.yellow);
+            Console.WriteLine("yellow={0}", (int)color.yellow);
+            Console.WriteLine("brown={0}",(int)color.brown);
+            Console.WriteLine("blue={0}", (int)color.blue);
+            Console.WriteLine("black={0}", (int)color.black);
+            Console.WriteLine("purple={0}", (int)color.purple);
+
+            Console.ReadKey();
         }
     }
 }
